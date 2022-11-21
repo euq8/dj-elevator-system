4a5ded889ee5c2f68fd8eaadddb6a9479397784d
Ashfaque Hussain <ashfaque.hussain@innovaccer.com>
2022-11-21 14:16:06 +0530

    Add Elevator and Request models and related enums

---
+++ elevatorAPIs/elevator_utils.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+
+class AbstractEnum(Enum):
+    @classmethod
+    def choices(cls):
+        return [
+            (enum_item.value, enum_item.value) 
+            for enum_item in cls.__members__.values()
+        ]
+
+
+class State(AbstractEnum):
+    MOVING = "moving"
+    STOPPED = "stopped"
+    IDLE = "idle"
+
+
+class Direction(AbstractEnum):
+    UP = "up"
+    DOWN = "down"
+
+
+class RequestType(AbstractEnum):
+    EXT = "external"
+    INT = "internal"
--- elevatorAPIs/models.py
+++ elevatorAPIs/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from elevatorAPIs.elevator_utils import (
+    State, Direction, RequestType
+)
 
-# Create your models here.
+TOP_FLOOR = 20 # assumption
+BOTTOM_FLOOR = -2 # assumption
+
+
+class Elevator(models.Model):
+    id = models.AutoField(primary_key=True)
+    current_floor = models.IntegerField()
+    current_state = models.CharField(
+        max_length=10,
+        choices=State.choices(),
+        default=(State.IDLE.value, State.IDLE.value),
+    )
+    current_direction = models.CharField(
+        max_length=10,
+        choices=Direction.choices(),
+        default=(Direction.UP.value, Direction.UP.value),
+    )
+    is_doors_closed = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "elevators"
+
+
+class Request(models.Model):
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=20, choices=RequestType.choices())
+    info = models.JSONField(blank=False)
+    elevator_id = models.ForeignKey(Elevator, to_field="id", on_delete=models.CASCADE)
+    is_completed = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "requests"
