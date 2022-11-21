15d5c357937e6cfef2ecfc37ed0e91a600f88247
Ashfaque Hussain <ashfaque.hussain@innovaccer.com>
2022-11-21 15:48:39 +0530

    Configure elevatorSystem/settings.py to connect to PostgreSQL's elvsys schema.

--- elevatorSystem/settings.py
+++ elevatorSystem/settings.py
@@ -77,9 +77,20 @@ WSGI_APPLICATION = 'elevatorSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'OPTIONS' : {
+            'options': '-c search_path=elvsys' # for custom schema
+        },
+        'NAME': 'dj_db', 
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
+    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
